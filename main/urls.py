945a4abd5d1e4bf4a75736ab941aa131a1fffa9a
DingYu <congrongdeyu@gmail.com>
2017-09-11 18:03:21 +0800

    edit pages

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^$', views.about, name='about'),
-    url(r'^$', views.news, name='news'),
-    url(r'^$', views.products, name='products'),
-    url(r'^$', views.hr, name='hr'),
-    url(r'^$', views.contacts, name='contacts'),
+    url(r'^index', views.index, name='index'),
+    url(r'about', views.about, name='about'),
+    url(r'news', views.news, name='news'),
+    url(r'products', views.products, name='products'),
+    url(r'hr', views.hr, name='hr'),
+    url(r'contacts', views.contacts, name='contacts'),
 ]
