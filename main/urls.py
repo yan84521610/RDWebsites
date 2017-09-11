310c6a2544478bb822ac29d72570f82b412e6176
DingYu <congrongdeyu@gmail.com>
2017-09-11 17:35:37 +0800

    edit pages

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='main'),
+    url(r'^$', views.index, name='index'),
+    url(r'^$', views.about, name='about'),
+    url(r'^$', views.news, name='news'),
+    url(r'^$', views.products, name='products'),
+    url(r'^$', views.hr, name='hr'),
+    url(r'^$', views.contacts, name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/main.html',)
+    return render(request, 'main/index.html', )
+
+def about(request):
+    return render(request, 'main/about.html', )
+
+def news(request):
+    return render(request, 'main/news.html', )
+
+def products(request):
+    return render(request, 'main/products.html', )
+
+def hr(request):
+    return render(request, 'main/hr.html', )
+
+def contacts(request):
+    return render(request, 'main/contacts.html', )
