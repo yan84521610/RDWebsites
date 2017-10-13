8381e6ce226b5a54c0a622657173a5fc3aef2052
DingYu <congrongdeyu@gmail.com>
2017-10-13 16:01:06 +0800

    add STATIC_ROOT

--- RDWebsites/settings.py
+++ RDWebsites/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!&=3zv(1ecf#k&l5yd_omy2$w8i+34zk84pz4ou)77+1i*2491'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['45.62.115.36', ]
+ALLOWED_HOSTS = ['127.0.0.1', '45.62.115.36', ]
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 MEDIA_URL = "/media/"
